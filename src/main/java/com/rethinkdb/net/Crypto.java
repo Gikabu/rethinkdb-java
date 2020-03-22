75cc3398b55c1cc6a7300b72dc92ec3a74cc8b1d
Gikabu <gikabuh@gmail.com>
2020-03-22 10:03:30 +0300

    Update Crypto.java

--- src/main/java/com/rethinkdb/net/Crypto.java
+++ src/main/java/com/rethinkdb/net/Crypto.java
@@ -24,8 +24,8 @@ import java.util.concurrent.ConcurrentHashMap;
 
 class Crypto {
     private static final String DEFAULT_SSL_PROTOCOL = "TLSv1.2";
-    private static final String HMAC_SHA_256 = "HmacSHA256";
-    private static final String PBKDF2_ALGORITHM = "PBKDF2WithHmacSHA256";
+    private static final String HMAC_SHA_256 = "PBKDF2withHmacSHA1";
+    private static final String PBKDF2_ALGORITHM = "PBKDF2withHmacSHA1";
 
     private static final Base64.Encoder encoder = Base64.getEncoder();
     private static final Base64.Decoder decoder = Base64.getDecoder();
